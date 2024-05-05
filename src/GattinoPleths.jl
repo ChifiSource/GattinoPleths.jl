1d40b2382163b2c7eebc59b2338cf66ff9d493c1
emmy <52672675+emmaccode@users.noreply.github.com>
2024-05-05 14:13:02 -0600

    new style resource loading

--- src/GattinoPleths.jl
+++ src/GattinoPleths.jl
@@ -1,43 +1,49 @@
 module GattinoPleths
 using Gattino
-using Gattino.ToolipsSVG.ToolipsServables: htmlcomponent
+using Gattino.ToolipsSVG.ToolipsServables: AbstractComponent
 __precompile__()
 dir::String = @__DIR__
 res::String = dir * "/../resources"
 
 struct CloreplethResource
     uri::String
-    names::Vector{String}
     dim::Pair{Int64, Int64}
+    names::Dict{String, Vector{Pair{String, String}}}
 end
 
-const world_map = CloreplethResource(res * "/world.svg", ["mx", "us", "ca", "sd", "ss", "ge", "pe", "bf", "ly", "by", 
-"pk", "id", "ye", "mg", "bo", "rs", "ci", "dz", "ch", "cm", "mk", "bw", "ke", "jo", 
-"mx", "ae", "bz", "br", "sl", "ml", "cd", "it", "so", "af", "bd", "do", "gw", "gh", "at", "se", "tr", 
-"ug", "mz", "jp", "nz", "cu", "ve", "pt", "co", "mr", "ao", "de", "th", "pg", "hr", 
-"gl", "ne", "dk", "lv", "ro", "zm", "mm", "et", "gt", "sr"], 3000 => 3000)
+def_names = Dict{String, Vector{Pair{String, String}}}("landxx" => ["fill"  => "lightgray", "stroke" => "#333333", "stroke-width" => ".5"], 
+"oceanxx" => ["fill" => "lightblue"],
+"coastxx" => ["fill"  => "lightgray", "stroke" => "#333333", "stroke-width" => ".5"], 
+"subxx" => ["fill"  => "lightgray", "stroke" => "#333333", "stroke-width" => ".5"], 
+"antxx" => ["fill"  => "white", "stroke" => "blue", "stroke-width" => ".5"], 
+"noxx" => ["fill"  => "white", "stroke" => "blue", "stroke-width" => ".5"],
+"limitxx" => ["fill"  => "white", "stroke" => "blue", "stroke-width" => ".5"], 
+"unxx" => ["opacity" => "0%"], 
+"eu" => ["fill"  => "blue", "stroke" => "#333333", "stroke-width" => ".5"], 
+"circlexx" => ["opacity" => "0%"])
+
+const world_map = CloreplethResource(res * "/world.svg", 2754 => 1398, def_names)
 
 function clorepleth(x::Vector{String}, y::Vector{<:Number}, rs::CloreplethResource, colors::Vector{String} = ["red", "pink"])
-    maxy = maximum(y)
+    maxy::Number = maximum(y)
     pleth::Context = context(rs.dim[1], rs.dim[2]) do con::Context
-        con.window[:children] = htmlcomponent(read(rs.uri, String), rs.names)
-        allnames::Vector{String} = [begin
-            comp[:text] = replace(comp[:text], "\"" => "'")
-            comp.name::String
-        end for comp in con.window[:children]]
-        n::Integer = length(colors)
-        nodata = filter(n -> ~(n in x), allnames)
-        [begin
-            current_y::Number = y[e]
-            perc = Int64(round((current_y / maxy) * n))
-            style!(con, value, "fill" => colors[perc])
-        end for (e, value) in enumerate(x)]
-        [begin 
-            style!(con, value, "fill" => "gray")
-        end for value in nodata]
+        con.window[:text] = replace(read(rs.uri, String), "\"" => "'")
     end
+    pleth.window[:children] = Vector{AbstractComponent}([begin 
+        Gattino.ToolipsSVG.Style("." * def, rs.names[def] ...)
+    end for def in keys(rs.names)])
+    n::Int64 = length(colors)
+    [begin
+        found = Int64(ceil((y[e] / maxy) * n))
+        push!(pleth.window, Gattino.ToolipsSVG.Style("." * def, "fill" => colors[found]))
+    end for (e, def) in enumerate(x)]
+    scale!(pleth, rs.dim[1], rs.dim[2])
     pleth::Context
 end
 
-export clorepleth
+function scale!(con::Context, w::Int64, h::Int64, x::Int64 = 0, y::Int64 = 0)
+    con.window["viewBox"] = "$x $y $w $h"
+end
+
+export clorepleth, scale!
 end # module GattinoPleths
